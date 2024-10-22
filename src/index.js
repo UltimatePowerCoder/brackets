219b471499009e4e06dffcd18802ea976ac8d566
Leonid Korshunov <ultimatepowercoder@gmail.com>
2024-10-22 16:31:30 +0400

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const bracketsMap = new Map(bracketsConfig);
+
+  for (let char of str) {
+      if (bracketsMap.has(char)) {
+          const isSame = bracketsMap.get(char) === char;
+          if (isSame && stack.includes(char)) {
+              stack.pop();
+          } else {
+              stack.push(char);
+          }
+      } else {
+          const openingBracket = [...bracketsMap.keys()].find(key => bracketsMap.get(key) === char);
+          if (openingBracket && stack.pop() !== openingBracket) {
+              return false;
+          }
+      }
+  }
+  
+  return stack.length === 0;
 }
